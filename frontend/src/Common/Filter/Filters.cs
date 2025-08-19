fbb94190dffd006247a0e427f697480953d1dccc
Pedro-HenridS <pedrosouza9479@gmail.com>
2025-08-19 19:41:24 -0300

    feat: adicionando filtros e melhorando estetica

---
+++ frontend/src/Common/Filter/Filters.cs
@@ -0,0 +1,11 @@
+﻿using src.Common.Enums.Pilot;
+
+namespace src.Common.Filter
+{
+    public class Filters
+    {
+        public string? Team { get; set; } = null;
+        public Gender? Gender { get; set; } = null;
+        public string? Nationality { get; set; } = null;
+    }
+}
